ee622e4289ad60fad01a262781483fb923e760d6
rugved18 <rugvedkulk2003@gmail.com>
2024-08-09 21:40:05 +0530

    updated

--- mini project/mini project/mini project/mini project/send/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import AnimalReport
-
-class AnimalReportAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone', 'location', 'animal_type', 'priority', 'latitude', 'longitude')
-
-admin.site.register(AnimalReport, AnimalReportAdmin)
--- mini project/mini project/mini project/mini project/send/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import AnimalReport
-
-class AnimalReportForm(forms.ModelForm):
-    class Meta:
-        model = AnimalReport
-        fields = '__all__'
